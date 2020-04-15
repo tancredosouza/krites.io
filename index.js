f6b83a8b33c265935a3e2661c2a083c5421494fd
Tancredo Souza <aqueletantan@gmail.com>
2020-04-15 11:59:02 -0300

    Display markdown content on webpage from local file.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const markdown = require("markdown").markdown;
 const app = express();
+const fs = require("fs");
+
+const md_content = fs.readFileSync("sample_file.md").toString();
+
+html_content = markdown.toHTML(md_content);
 
 app.get("/", (req, res) => {
-  res.send("Hello, there");
+  res.send(html_content);
 });
 
 app.listen(8080, () => {
---
+++ sample_file.md
@@ -0,0 +1,5 @@
+# This is a title
+
+Is this working? I do not know.
+
+# Goodbye!
