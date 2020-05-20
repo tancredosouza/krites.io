e426372a29a8c6835e18e92e4b60c5f2ef550d93
Tancredo Souza <aqueletantan@gmail.com>
2020-05-20 15:36:25 -0300

    Updates

---
+++ precache-manifest.730c4ca327ceb03f596174f2cdc50a9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396d23fc3a7e727603822ecde8b12678",
+    "url": "/krites_io/index.html"
+  },
+  {
+    "revision": "b9c2d067bfed8d4acb7d",
+    "url": "/krites_io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/krites_io/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krites_io/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c2d067bfed8d4acb7d",
+    "url": "/krites_io/static/js/main.a374231d.chunk.js"
+  },
+  {
+    "revision": "de9015bfb53b528d8925",
+    "url": "/krites_io/static/js/runtime-main.aa0584b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/krites_io/static/media/logo.5d5d9eef.svg"
+  }
+]);
